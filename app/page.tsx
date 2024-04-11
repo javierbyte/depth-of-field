b3e715a1bfbf9ad173291b323d86135b42d9c820
javierbyte <hi@javier.xyz>
2024-04-11 04:27:04 +0200

    Refine code, add Todossantos

--- app/page.tsx
+++ app/page.tsx
@@ -12,21 +12,19 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const CURSOR_SENSITIVITY = 0.333;
-const CSS_PERSPECTIVE = 1280;
+const CSS_PERSPECTIVE = 1080;
 const MAX_BLUR = 16;
 const SPRING_TENSION = 0.8;
-const SENSITIVITY_Y = 0.618;
 const PAN_RANGE = -100;
 
 export default function Home() {
   const dataRef = useRef({
     globalAperture: 1.4,
     forceRender: false,
-    targetX: 0.1,
-    targetY: 0.4,
-    renderX: 0.5,
-    renderY: 0.5,
+    targetX: -0.22,
+    targetY: -0.1,
+    renderX: 0,
+    renderY: 0,
   });
   const [aperture, setAperture] = useState(1.4);
   const [photo, setPhoto] = useState<keyof typeof photos>("Berlin");
@@ -46,12 +44,18 @@ export default function Home() {
 
     const onCursorMove = (e: MouseEvent) => {
       const data = dataRef.current;
-      const width = window.innerWidth;
-      const height = window.innerHeight;
 
-      data.targetX = e.clientX / width;
-      data.targetY =
-        (e.clientY / height) * SENSITIVITY_Y + 0.5 * (1 - SENSITIVITY_Y);
+      const { innerWidth, innerHeight } = window;
+
+      const { width, height, left, top } = imgContainer.getBoundingClientRect();
+      const imgCenterX = left + width / 2;
+      const imgCenterY = top + height / 2;
+
+      const scaleX = innerHeight > innerWidth ? 1.25 : 1;
+
+      data.targetX =
+        ((e.clientX - imgCenterX) / (innerWidth + innerHeight)) * scaleX;
+      data.targetY = (e.clientY - imgCenterY) / (innerWidth + innerHeight);
     };
 
     function updateStyles() {
@@ -75,16 +79,14 @@ export default function Home() {
       data.renderY =
         data.renderY * SPRING_TENSION + data.targetY * (1 - SPRING_TENSION);
 
-      const x =
-        1 - data.renderX * CURSOR_SENSITIVITY - 0.5 * (1 - CURSOR_SENSITIVITY);
-      const y =
-        data.renderY * CURSOR_SENSITIVITY + 0.5 * (1 - CURSOR_SENSITIVITY);
+      const x = data.renderX * 0.618;
+      const y = data.renderY * 0.618;
 
-      const angle = Math.atan2(data.renderY - 0.5, data.renderX - 0.5);
-      const angleDeg = Math.round(((angle * 180) / Math.PI + 90) * 100) / 100;
+      const angle = Math.atan2(y, x);
+      const angleDeg = Math.round(((angle * 180) / Math.PI + 90) * 1000) / 1000;
 
-      const xDeg = Math.round((x * -180 + 90) * 100) / 100;
-      const yDeg = Math.round((y * -180 + 90) * 100) / 100;
+      const xDeg = Math.round(x * 180 * 1000) / 1000;
+      const yDeg = Math.round(-y * 180 * 1000) / 1000;
 
       const isSmallScreen = window.innerWidth < 768;
 
@@ -92,68 +94,74 @@ export default function Home() {
         imgContainer.style.transform = `perspective(${CSS_PERSPECTIVE}px) rotateX(${yDeg}deg) rotateY(${xDeg}deg)`;
       } else {
         imgContainer.style.transform = `translatex(${
-          (-x + 0.5) * PAN_RANGE
+          x * PAN_RANGE
         }px) translatey(${
-          (-y + 0.5) * -PAN_RANGE
+          -y * -PAN_RANGE
         }px) perspective(${CSS_PERSPECTIVE}px) rotateX(${yDeg}deg) rotateY(${xDeg}deg)`;
       }
 
       const blurIntensity =
         (Math.sqrt(Math.pow(xDeg, 2) + Math.pow(yDeg, 2)) /
           data.globalAperture) *
-        0.5;
+          0.4 -
+        1;
 
-      const finalBlur = Math.max(Math.min(blurIntensity, MAX_BLUR) - 1, 0);
+      const finalBlur = Math.max(Math.min(blurIntensity, MAX_BLUR), 0);
 
       const mask = `linear-gradient(
         ${angleDeg}deg,
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 0)
+        rgba(0, 0, 0, 0) 0%,
+        rgba(0, 0, 0, 0) 20%,
+        rgba(0, 0, 0, 1) 35%,
+        rgba(0, 0, 0, 1) 65%,
+        rgba(0, 0, 0, 0) 80%,
+        rgba(0, 0, 0, 0) 100%
       )`;
+
       img.style.maskImage = mask;
 
       const mediumMask =
         data.globalAperture < 1
           ? `linear-gradient(
         ${angleDeg}deg,
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1)
+        rgba(0, 0, 0, 1) 0%,
+        rgba(0, 0, 0, 1) 40%,
+        rgba(0, 0, 0, 0) 48%,
+        rgba(0, 0, 0, 0) 53%,
+        rgba(0, 0, 0, 1) 60%,
+        rgba(0, 0, 0, 1) 100%
       )`
           : `linear-gradient(
         ${angleDeg}deg,
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1)
+        rgba(0, 0, 0, 1) 0%,
+        rgba(0, 0, 0, 1) 35%,
+        rgba(0, 0, 0, 0) 45%,
+        rgba(0, 0, 0, 0) 55%,
+        rgba(0, 0, 0, 1) 65%,
+        rgba(0, 0, 0, 1) 100%
       )`;
       imgMedium.style.maskImage = mediumMask;
-      imgMedium.style.filter = `blur(${finalBlur / 2}px)`;
+      imgMedium.style.filter = `blur(${finalBlur * 0.5}px)`;
 
-      const maskFar = `linear-gradient(
+      const maskFar =
+        data.globalAperture < 1
+          ? `linear-gradient(
         ${angleDeg}deg,
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 0),
-        rgba(0, 0, 0, 1),
-        rgba(0, 0, 0, 1)
+        rgba(0, 0, 0, 1) 0%,
+        rgba(0, 0, 0, 1) 25%,
+        rgba(0, 0, 0, 0) 35%,
+        rgba(0, 0, 0, 0) 65%,
+        rgba(0, 0, 0, 1) 75%,
+        rgba(0, 0, 0, 1) 100%
+      )`
+          : `linear-gradient(
+        ${angleDeg}deg,
+        rgba(0, 0, 0, 1) 0%,
+        rgba(0, 0, 0, 1) 20%,
+        rgba(0, 0, 0, 0) 30%,
+        rgba(0, 0, 0, 0) 70%,
+        rgba(0, 0, 0, 1) 80%,
+        rgba(0, 0, 0, 1) 100%
       )`;
       imgFar.style.maskImage = maskFar;
       imgFar.style.filter = `blur(${finalBlur}px)`;
@@ -264,11 +272,14 @@ const photos = {
   Berlin: {
     src: ["/berlin-1.jpg", "/berlin-1.jpg", "/berlin-1.jpg"],
   },
+  Schwerin: {
+    src: ["/schwerin-1.jpg", "/schwerin-1.jpg", "/schwerin-1.jpg"],
+  },
   Brutalita: {
     src: ["/brutalita-1.jpg", "/brutalita-1.jpg", "/brutalita-1.jpg"],
   },
-  Schwerin: {
-    src: ["/schwerin-1.jpg", "/schwerin-1.jpg", "/schwerin-1.jpg"],
+  "Todos Santos": {
+    src: ["todos-santos-1.jpg", "todos-santos-1.jpg", "todos-santos-1.jpg"],
   },
   "Debug Layers": {
     src: ["/solid-1.png", "/solid-2.png", "/solid-3.png"],
